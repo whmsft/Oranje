8b1fd12bcf9c23217e968038a0140db258e64a77
whmsft <whmsft@outlook.com>
2023-04-20 20:06:41 +0530

    skeleton updates

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
 import chlorophyll
 import tklinenums
 import tkinter as tk
 from tkinter import ttk
 
+cwd = os.getcwd()
+
+PACKAGE = {}
+
+for author in os.listdir(cwd+"/package"): PACKAGE[author] = []
+for author in PACKAGE.keys(): 
+    for package in os.listdir(cwd+"/package/"+author): PACKAGE[author].append(package)
+print(PACKAGE)
