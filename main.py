fb1f475effd80c861d865f7df07f144c602ad15c
whmsft <whmsft@outlook.com>
2023-04-24 17:37:20 +0530

    fixed tiny bug with package loading

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("Welcome, This is Oranje Editor executable...")
 import os
 import yaml
 from package.whmsft import oranje
@@ -17,30 +18,35 @@ PACKAGE = {
 """
 
 if __name__ == "__main__":
+	print("(log) looking for packages")
 	for author in os.listdir(f'{pdir}/package'):
 		if os.path.isdir(f'{pdir}/package/{author}'):
-			PACKAGE[author] = []
+			PACKAGE[author] = {}
 	for author in PACKAGE.keys():
 		for pack in os.listdir(f'{pdir}/package/{author}'):
 			if (os.path.isdir(f'{pdir}/package/{author}/{pack}')):
 				if not (author == "whmsft" and pack == "oranje"):
+					print(f"(log) PACKAGE: found {author}.{pack}")
 					PACKAGE[author][pack] = yaml.safe_load(open(f'{pdir}/package/{author}/{pack}/data.yml').read())
 					exec(f'import package.{author}.{pack}')
 					taskList = PACKAGE[author][pack]['tasks']
 					if ('initialize' in taskList) and (taskList['initialize'] != None):
+						print(f"(log) PACKAGE: initialize package {author}.{pack}")
 						exec(f'package.{author}.{pack}.{taskList["initialize"]}')
 					del taskList
 	
 	for author in PACKAGE.keys():
-		for pack in author.keys():
+		for pack in PACKAGE[author].keys():
 			taskList = PACKAGE[author][pack]['tasks']
 			if ('beforeLoop' in taskList) and (taskList['beforeLoop'] != None):
+				print(f"(log) PACKAGE: beforeLoop package {author}.{pack}")
 				exec(f'package.{author}.{pack}.{taskList["beforeLoop"]}')
 			del taskList
 	oranje.editor.mainloop()
 	for author in PACKAGE.keys():
-		for pack in author.keys():
+		for pack in PACKAGE[author].keys():
 			taskList = PACKAGE[author][pack]['tasks']
 			if ('afterLoop' in taskList) and (taskList['afterLoop'] != None):
+				print(f"(log) PACKAGE: afterLoop package {author}.{pack}")
 				exec(f'package.{author}.{pack}.{taskList["afterLoop"]}')
 			del taskList
--- package/whmsft/oranje/__init__.py
+++ package/whmsft/oranje/__init__.py
@@ -8,5 +8,5 @@ editor = tk.Tk()
 ttk.Style().configure("TLineNumbers", background="#202020", foreground="#ffffff")
 codeview = CodeView(editor, lexer=lexers.PythonLexer, color_scheme="monokai", font="Cascadia\ Code 14", tab_width=4)
 codeview.pack(fill="both", expand=True)
-
+editor.after(10, editor.quit)
 if __name__ == "__main__": editor.mainloop()
