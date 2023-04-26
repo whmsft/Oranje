fc8c1dbced2821d73aecdf251a550a3d1433b934
whmsft <whmsft@outlook.com>
2023-04-26 16:10:42 +0530

    rewrote the downloader

--- ore.py
+++ ore.py
@@ -1,25 +1,23 @@
 # Ore - A package manager for Oranje
+import sys
 import requests
 from tqdm import tqdm
 
-# The URL of the GitHub repository to download
-url = "https://github.com/whmsft/oranje/archive/refs/heads/main.zip"
+if len(sys.argv) > 1:
+    repo = sys.argv[1]
+    branch = sys.argv[2] if (len(sys.argv)>2) else "main"
+else: sys.exit()
 
-# The path and filename to save the downloaded file to
-filename = "oranje.zip"
+url = f'https://github.com/{repo}/archive/refs/heads/{branch}.zip'
+filename = f'./package/{repo}.zip'
 
-# Send a GET request to the URL and get the content length of the response
 response = requests.get(url, stream=True)
 file_size = int(response.headers.get("Content-Length", 0))
 
-# Create a tqdm progress bar and start downloading the file
 with open(filename, "wb") as f:
     with tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024) as progress:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:
-                # Write the downloaded data to the file and update the progress bar
                 f.write(chunk)
                 progress.update(len(chunk))
-
-# Print a message to indicate that the download is complete
 print("Download complete!")
