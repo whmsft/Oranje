6ea9edd80129810f9b90d2d0d7ad3a63a635295f
whmsft <whmsft@outlook.com>
2023-04-29 15:39:58 +0530

    Add Oranje Editor initialization code and data file

---
+++ package/whmsft/oranje/__init__.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+from pygments import lexers
+from chlorophyll import CodeView
+
+editor = tk.Tk()
+
+ttk.Style().configure("TLineNumbers", background="#202020", foreground="#ffffff")
+codeview = CodeView(editor, lexer=lexers.PythonLexer, color_scheme="monokai", font="Cascadia\ Code 14", tab_width=4)
+codeview.pack(fill="both", expand=True)
+if __name__ == "__main__": editor.mainloop()
